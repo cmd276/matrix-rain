4495c2638e6bb69d7bac771347cd949509e19fe1
Jan Vermeulen <sigstart@entropy.co.za>
2018-04-10 09:39:33 +0200

    Removed dependency on jQuery.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var c = document.getElementById("c");
+var c = document.getElementById("matrix");
 var ctx = c.getContext("2d");
 
 // making the canvas full screen
@@ -17,7 +17,7 @@ var columns = c.width/font_size;    // number of columns for the rain
 var drops = [];
 // x below is the x coordinate
 // 1 = y-coordinate of the drop (same for every drop initially)
-for (var x = 0; x < columns; x++) 
+for (var x = 0; x < columns; x++)
     drops[x] = 1;
 
 function getColor() {
@@ -27,39 +27,33 @@ function getColor() {
 }
 
 function getColorHex() {
-    return "#" + moment().format('HHmmss');   
+    return "#" + moment().format('HHmmss');
 }
 
 // drawing the characters
 function draw() {
-    $("#color").css("background", getColorHex());
-    $('#color').html(getColorHex());
-    
     // Get the BG color based on the current time i.e. rgb(hh, mm, ss)
     // translucent BG to show trail
     ctx.fillStyle = getColor();
     ctx.fillRect(0, 0, c.width, c.height);
-    
+
     ctx.fillStyle = "#BBB"; // grey text
     ctx.font = font_size + "px arial";
-    
+
     // looping over drops
     for (var i = 0; i < drops.length; i++) {
-        // a random chinese character to print
+        // a random character to print
         var text = characters[Math.floor(Math.random() * characters.length)];
         // x = i * font_size, y = value of drops[i] * font_size
         ctx.fillText(text, i * font_size, drops[i] * font_size);
-        
+
         // sending the drop back to the top randomly after it has crossed the screen
         // adding randomness to the reset to make the drops scattered on the Y axis
         if (drops[i] * font_size > c.height && Math.random() > 0.975)
             drops[i] = 0;
-        
+
         // Incrementing Y coordinate
         drops[i]++;
     }
 }
-
-
-
 setInterval(draw, 33);
